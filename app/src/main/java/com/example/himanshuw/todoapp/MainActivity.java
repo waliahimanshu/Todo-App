bd00c127b1205c1f0c080f158c7aa9f197cc1fb1
himanshuw <himanshu.walia@thetrainline.com>
2016-06-24 17:42:05 +0100

    button click stores items in ArrayList

--- app/src/main/java/com/example/himanshuw/todoapp/MainActivity.java
+++ app/src/main/java/com/example/himanshuw/todoapp/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.himanshuw.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void AddItem(View view)
+    {
+        List<String> stringList = new ArrayList<String>();
+
+        EditText  editText =(EditText)findViewById(R.id.editTextView);
+
+        String text = editText.getText().toString();
+
+        stringList.add(text);
+
+        for (String item : stringList)
+        {
+            Log.i("Test","---------->>"+item);
+        }
+
     }
 }
